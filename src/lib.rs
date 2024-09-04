619a7408d3f748e50f95f2b557f2905a86dc3b24
Chandler Calkins <chandlerjaycalkins@gmail.com>
2024-09-04 16:13:15 -0700

    Added documentation comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,59 @@
+//! Rust attribute macro library that makes it easy to wrap functions in code that runs before and / or after a function executes.
+//!
+//! This function
+//!
+//! ```rs
+//! #[wrap]
+//! fn hello() -> bool
+//! {
+//! 	println!("Hello there!");
+//! 	println!("This is some code.");
+//! 	true
+//! }
+//! ```
+//!
+//! which is being wrapped by this attribute
+//!
+//! ```rs
+//! use function_wrapper::WrappedFn;
+//! extern crate proc_macro;
+//! extern crate proc_macro2;
+//! use syn::parse_macro_input;
+//! use quote::quote;
+//!
+//! // Adds print statements before and after a function executes.
+//! #[proc_macro_attribute]
+//! pub fn wrap(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream
+//! {
+//! 	// Parse input as a WrappedFn object from the function-wrapper crate.
+//! 	let mut function = parse_macro_input!(item as WrappedFn);
+//! 	// Put a print statement in the code that gets run before the function.
+//! 	function.set_pre_code(quote!{ println!("Hi at the start :)"); });
+//! 	// Put a print statement in the code that gets run after the function.
+//! 	function.set_post_code(quote!{ println!("Hi at the end :)"); });
+//! 	// Convert the function into a TokenStream and return it.
+//! 	proc_macro2::TokenStream::from(function).into()
+//! }
+//! ```
+//!
+//! will turn into this after being compiled.
+//!
+//! ```rs
+//! fn hello() -> bool
+//! {
+//! 	println!("Hi at the start :)");
+//! 	let mut wrapper = ||
+//! 	{
+//! 		println!("Hello there!");
+//! 		println!("This is some code.");
+//! 		true
+//! 	};
+//! 	let result = wrapper();
+//! 	println!("Hi at the end :)");
+//! 	result
+//! }
+//! ```
+
 use proc_macro2::{TokenStream, Span};
 use syn::{ItemFn, Ident, /* Type, ReturnType */};
 use syn::parse::{Parse, ParseStream};
@@ -24,10 +80,32 @@ const ERROR_STRS: [&str; 1] =
 
 /// Function that can have code inserted before and after the rest of the function executes.
 /// Can be constructed with `syn::parse()` and other variations of parsing from the `syn` crate.
+///
+/// Example:
+///
+/// ```rs
+/// let mut function = parse_macro_input!(token_stream as WrappedFn);
+/// ```
+///
+/// Code that runs before the function can be set using the `set_pre_code()` method.
+///
+/// Example:
+///
+/// ```rs
+/// function.set_pre_code(quote!{ println!("Hi at the start :)"); });
+/// ```
+///
+/// Code that runs after the function can be set using the `set_post_code()` method.
+///
+/// Example:
+///
+/// ```rs
+/// function.set_post_code(quote!{ println!("Hi at the end :)"); });
+/// ```
 #[derive(Clone)]
 pub struct WrappedFn
 {
-	/// `proc_macro2::TokenStream` that contains code that gets run before the rest of the function.
+	/// Contains code that gets run before the rest of the function.
 	pub pre_code: Option<TokenStream>,
 	/// `syn::ItemFn` that contains all of the data of the original function, including the code inside, the function signature, any attributes, etc.
 	pub function: ItemFn,
@@ -35,11 +113,11 @@ pub struct WrappedFn
 	// pub args: Vec<FnArgData>,
 	// /// Return type.
 	// pub output: WrappedFnOutput,
-	/// Identifier token for the closure that wraps all of the original code from the wrapped function.
+	/// Identifier token for the closure that wraps all of the original code from the wrapped function. It is `wrapper` by default.
 	pub wrapper_ident: Ident,
-	/// Identifier token for the variable that holds the return value of the wrapped function. Is almost always `result`.
+	/// Identifier token for the variable that holds the return value of the wrapped function. It is `result` by default.
 	pub result_ident: Ident,
-	/// `proc_macro2::TokenStream` that contains code that gets run after the rest of the function.
+	/// Contains code that gets run after the rest of the function.
 	pub post_code: Option<TokenStream>
 }
 
@@ -59,6 +137,11 @@ impl WrappedFn
 }
 
 /// Main way to construct a `WrappedFn`.
+/// Can be constructed using `syn::parse_macro_input` like this:
+///
+/// ```rs
+/// let mut function = parse_macro_input!(token_stream as WrappedFn);
+/// ```
 impl Parse for WrappedFn
 {
 	/// Constructs a WrappedFn from a `syn::ParseStream`.
