2076910888a1426487e4046f837d800d43e1a751
Chandler Calkins <chandlerjaycalkins@gmail.com>
2024-09-04 15:23:38 -0700

    Added comments to the test macro

--- tests/macros/src/lib.rs
+++ tests/macros/src/lib.rs
@@ -4,18 +4,22 @@ extern crate proc_macro2;
 use syn::{parse_macro_input, FnArg};
 use quote::quote;
 
-// Attribute that tests adding code before and after the rest of a function
+// Attribute that tests adding code before and after the rest of a function.
 #[proc_macro_attribute]
 pub fn wrap_test(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream
 {
+	// Parses the input as a WrappedFn so code can be added before and / or after the rest of the function executes.
 	let mut function = parse_macro_input!(item as WrappedFn);
+	// Get a proc_macro2::TokenStream of the code that runs before the function.
 	let mut start_code = quote!
 	{
 		println!("########################################");
 		println!("hi at the start");
 	};
+	// Add a print statement for each argument to the function.
 	for arg in &function.function.sig.inputs
 	{
+		// Determine whether the argument is a receiver (self arg) or a normal arg.
 		match arg
 		{
 			FnArg::Receiver(receiver) => start_code = quote!{ #start_code println!("{:?}", #receiver); },
@@ -26,16 +30,21 @@ pub fn wrap_test(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> p
 			}
 		}
 	}
+	// Apply the pre_code to the wrapped function.
 	function.set_pre_code(start_code);
+	// Get the identifier of the variable that holds the return value of the function.
 	let result_ident = &function.result_ident;
+	// Set the code that runs after the function to be a print statement of the return value and some other print statements.
 	function.set_post_code(quote!
 	{
 		println!("return value: {:?}", #result_ident);
 		println!("hi at the end");
 		println!("########################################");
 	});
+	// Convert the function into a proc_macro2::TokenStream
 	let ts = proc_macro2::TokenStream::from(function);
 	println!("{}", ts.clone());
+	// Return the TokenStream of the function
 	ts.into()
 }
 
